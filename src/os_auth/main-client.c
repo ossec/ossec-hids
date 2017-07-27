187a0ffa7a9746376479590eb9108a90ef5b0961
ddpbsd <ddpbsd@gmail.com>
2017-07-27 16:05:10 -0400

    Revert "Pulled these from pull request #1178 by @hcw2016"
    This reverts commit 44cb77705ea72c539b319116d708af844ddfa6e4.

--- src/os_auth/main-client.c
+++ src/os_auth/main-client.c
@@ -252,7 +252,7 @@ int main(int argc, char **argv)
         }
     }
     if (!authpass) {
-        printf("WARN: No authentication password provided.\n");
+        printf("WARN: No authentication password provided. Insecure mode started.\n");
     }
 
     /* Connect via TCP */
