d7b3ec7c6f4cf9c32746535278b31a00e574a84a
ddpbsd <ddpbsd@gmail.com>
2018-09-19 08:12:17 -0400

    Fix my sed

--- contrib/version_bump.sh
+++ contrib/version_bump.sh
@@ -22,15 +22,15 @@ fi
 echo "v${NEWVERSION}" > src/VERSION
 
 # OSSEC init scripts
-sed -ie "s/VERSION=\"v${OLDVERSION}/VERSION=\"v${NEWVERSION}/" src/init/ossec-client.sh
-sed -ie "s/VERSION=\"v${OLDVERSION}/VERSION=\"v${NEWVERSION}/" src/init/ossec-local.sh
-sed -ie "s/VERSION=\"v${OLDVERSION}/VERSION=\"v${NEWVERSION}/" src/init/ossec-server.sh
+sed -i -e "s/VERSION=\"v${OLDVERSION}/VERSION=\"v${NEWVERSION}/" src/init/ossec-client.sh
+sed -i -e "s/VERSION=\"v${OLDVERSION}/VERSION=\"v${NEWVERSION}/" src/init/ossec-local.sh
+sed -i -e "s/VERSION=\"v${OLDVERSION}/VERSION=\"v${NEWVERSION}/" src/init/ossec-server.sh
 
 # Win32 files
-sed -ie "s/VERSION \"${OLDVERSION}/VERSION \"${NEWVERSION}/" src/win32/ossec-installer.nsi
-sed -ie "s/Agent v${OLDVERSION}/Agent v${NEWVERSION}/" src/win32/help.txt
+sed -i -e "s/VERSION \"${OLDVERSION}/VERSION \"${NEWVERSION}/" src/win32/ossec-installer.nsi
+sed -i -e "s/Agent v${OLDVERSION}/Agent v${NEWVERSION}/" src/win32/help.txt
 
 # misc
-sed -ie "s/OSSEC v${OLDVERSION}/OSSEC v${NEWVERSION}/" INSTALL
-sed -ie "s/OSSEC v${OLDVERSION}/OSSEC v${NEWVERSION}/" README.md
+sed -i -e "s/OSSEC v${OLDVERSION}/OSSEC v${NEWVERSION}/" INSTALL
+sed -i -e "s/OSSEC v${OLDVERSION}/OSSEC v${NEWVERSION}/" README.md
 
