509222770d728924be65d9086b2c69a64710ffee
ddpbsd <ddpbsd@gmail.com>
2019-01-02 17:23:35 -0500

    I'm not sure why I thought strncat worked like that.

--- src/os_crypto/md5_sha1/md5_sha1_op.c
+++ src/os_crypto/md5_sha1/md5_sha1_op.c
@@ -190,28 +190,28 @@ int OS_Hash_File(const char *fname, const char *prefilter_cmd, struct hash_outpu
         for (n = 0; n < 16; n++) {
             hashtmp[0] = '\0';
             snprintf(hashtmp, 3, "%02x", md5_digest[n]);
-            strncat(file_output->md5output, hashtmp, sizeof(file_output->md5output) - 1 - strlen(file_output->md5output));
+            strncat(file_output->md5output, hashtmp, 2);
         }
     }
     if(file_output->check_sha1) {
         for (n = 0; n < 16; n++) {
             hashtmp[0] = '\0';
             snprintf(hashtmp, 3, "%02x", sha1_digest[n]);
-            strncat(file_output->sha1output, hashtmp, sizeof(file_output->sha1output) - 1 - strlen(file_output->sha1output));
+            strncat(file_output->sha1output, hashtmp, 2);
         }
     }
     if(file_output->check_generic) {
         for (n = 0; n < crypto_generichash_BYTES_MAX; ++n) {
             hashtmp[0] = '\0';
             snprintf(hashtmp, 3, "%02x", generic_digest[n]);
-            strncat(file_output->genericoutput, hashtmp, sizeof(file_output->genericoutput) - 1 - strlen(file_output->genericoutput));
+            strncat(file_output->genericoutput, hashtmp,2);
         }
     }
     if(file_output->check_sha256) {
         for (n = 0; n < crypto_hash_sha256_BYTES; ++n) {
             hashtmp[0] = '\0';
             snprintf(hashtmp, 3, "%02x", sha256_digest[n]);
-            strncat(file_output->sha256output, hashtmp, sizeof(file_output->sha256output) - 1 - strlen(file_output->sha256output));
+            strncat(file_output->sha256output, hashtmp, 2);
         }
     }
 
