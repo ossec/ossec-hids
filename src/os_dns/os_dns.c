bb9ca45e0e0c37e50c043cb761515121381b73af
ddpbsd <ddpbsd@gmail.com>
2019-04-16 14:20:03 -0400

    I think this should work with multiple servers now.

--- src/os_dns/os_dns.c
+++ src/os_dns/os_dns.c
@@ -43,10 +43,6 @@ void osdns_accept(int fd, short ev, void *arg) {
     /* sssssssh */
     if (fd) { }
 
-    /* temporary things */
-    char *protocol = "tcp";
-
-
     /* We have a request from ossec-maild */
     struct os_dns_request dnsr;
     ssize_t n, datalen;
@@ -62,7 +58,6 @@ void osdns_accept(int fd, short ev, void *arg) {
     if (ev & EV_READ) {
         if ((n = imsg_read(ibuf)) == -1 && errno != EAGAIN) {
             ErrorExit("%s [dns]: ERROR: imsg_read() failed: %s", dname, strerror(errno));
-            return;
         }
         if (n == 0) {
             debug2("%s [dns]: DEBUG: n == 0", dname);
@@ -86,21 +81,25 @@ void osdns_accept(int fd, short ev, void *arg) {
         datalen = imsg.hdr.len - IMSG_HEADER_SIZE;
 
         switch(imsg.hdr.type) {
+            /*
+             * OS_Sendmail() sends a DNS_REQ for the smtp_server
+             * osdns() sends back a socket to the connection to the smtp_server
+             */
             case DNS_REQ:
                 if (datalen != sizeof(&dnsr)) {
-                    //XXX error
                     merror("%s [dns]: ERROR: DNS_REQ wrong length (%lu)", dname, datalen);
                     struct os_dns_error os_dns_err;
                     //os_dns_err.msg = "wrong len";
                     imsg_compose(ibuf, DNS_FAIL, 0, 0, -1, &os_dns_err, sizeof(&os_dns_err));
                     if ((n = msgbuf_write(&ibuf->w) == -1) && errno != EAGAIN) {
-                        merror("%s [dns]: ERROR: msgbuf_write() failed (DNS_FAIL size): %a", dname, strerror(errno));
+                        merror("%s [dns]: ERROR: msgbuf_write() failed (DNS_FAIL size): %s", dname, strerror(errno));
+                        return;
                     }
                     if (n == 0) {
                         debug2("%s [dns]: DEBUG: DNS_FAIL size n == 0", dname);
                     }
                     if (n == EAGAIN) {
-                        merror("%s [dns]: DEBUG EAGAIN size", dname);
+                        debug2("%s [dns]: DEBUG EAGAIN size", dname);
                     }
                 }
                 memcpy(&dnsr, imsg.data, sizeof(dnsr));
@@ -108,11 +107,7 @@ void osdns_accept(int fd, short ev, void *arg) {
                 struct addrinfo hints, *result, *rp = NULL;
                 memset(&hints, 0, sizeof(hints));
                 hints.ai_family = AF_UNSPEC;
-                if (strncmp(protocol, "tcp", 3) == 0) {
-                    hints.ai_socktype = SOCK_STREAM;
-                } else if (strncmp(protocol, "udp", 3) == 0) {
-                    hints.ai_socktype = SOCK_DGRAM;
-                }
+                hints.ai_socktype = SOCK_STREAM;
 
                 /* socket */
                 int sock;
@@ -132,7 +127,7 @@ void osdns_accept(int fd, short ev, void *arg) {
                         debug2("%s [dns]: DEBUG: DNS_FAIL n == 0", dname);
                     }
                     if (n == EAGAIN) {
-                        merror("%s [dns]: DEBUG: EAGAIN 1", dname);
+                        debug2("%s [dns]: DEBUG: EAGAIN 1", dname);
                     }
                 }
 
@@ -178,59 +173,65 @@ void osdns_accept(int fd, short ev, void *arg) {
                     debug2("%s [dns]: DEBUG: DNS_FAIL n == 0", dname);
                 }
                 if (n == EAGAIN) {
-                    merror("%s [dns]: DEBUG: EAGAIN 1", dname);
+                    debug2("%s [dns]: DEBUG: EAGAIN 1", dname);
                 }
                 break;
-            /*
-             * Until I find a better way to pass this stuff around,
-             * break out the agentd stuff from maild. It's not ideal, but
-             * it should work for now.
-             */
 #ifdef CLIENT
             case AGENT_REQ:
+                /*
+                 * agentd needs to find the IP of the ossec server
+                 * This should give agentd a socket to use for the traffic
+                 */
                 memcpy(&agt, imsg.data, sizeof(agt));
 
                 int rc = 0;
-                agt->sock = OS_ConnectUDP(agt->port, agt->rip[rc]);
-                if (agt->sock < 0) {
-                    agt->sock = -1;
-                    merror(CONNS_ERROR, dname, agt->rip[rc]);
-                    rc++;
-
-                    /* Fail */
-                    if (agt->rip[rc] == NULL) {
+                while (agt->rip[rc] != NULL) {
+                    agt->sock = OS_ConnectUDP(agt->port, agt->rip[rc]);
+                    if (agt->sock < 0) {
+                        agt->sock = -1;
+                        merror(CONNS_ERROR, dname, agt->rip[rc]);
                         attempts += 10;
-                        /* Only log that if we have more than 1 server configured */
-                        if (agt->rip[1]) {
-                            merror("%s: ERROR: Unable to connect to any server.", dname);
-                        }
-
                         sleep(attempts);
-                        rc = 0;
-                    }
-                } else {
-                    /* Success */
-                    /* Send the socket back to agentd */
-                    if ((imsg_compose(ibuf, DNS_RESP, 0, 0, agt->sock, &idata, sizeof(idata))) == -1) {
-                        merror("%s [dns]: ERROR: DNS_RESP imsg_compose() failed: %s", dname, strerror(errno));
-                        freeaddrinfo(result);
-                        return;
+                        if (rc > 0) {
+                            merror("Could not connect to any server.");
+                            struct os_dns_error os_dns_err;
+                            os_dns_err.code = -1;
+                            imsg_compose(ibuf, DNS_FAIL, 0, 0, -1, &os_dns_err, sizeof(&os_dns_err));
+                            if ((n = msgbuf_write(&ibuf->w) == -1) && errno != EAGAIN) {
+                                    merror("%s [dns]: ERROR: msgbuf_write() failed (AGENT_REQ)", dname);
+                                    return;
+                            }
+                            if (n == 0) {
+                                debug2("%s [dns]: DEBUG: AGENT_REQ size n == 0", dname);
+                                return;
+                            }
+                                    
+                        } else {
+                            rc++;
+                        }
                     } else {
-                        if ((n = msgbuf_write(&ibuf->w) == -1) && errno != EAGAIN) {
-                            merror("%s [dns]: ERROR: DNS_RESP msgbuf_write() failed: %s", dname, strerror(errno));
+                        /* Success */
+                        /* Send the socket back to agentd */
+                        if ((imsg_compose(ibuf, DNS_RESP, 0, 0, agt->sock, &idata, sizeof(idata))) == -1) {
+                            merror("%s [dns]: ERROR: DNS_RESP imsg_compose() failed: %s", dname, strerror(errno));
+                            freeaddrinfo(result);
+                            return;
+                        } else {
+                            if ((n = msgbuf_write(&ibuf->w) == -1) && errno != EAGAIN) {
+                                merror("%s [dns]: ERROR: DNS_RESP msgbuf_write() failed: %s", dname, strerror(errno));
+                                freeaddrinfo(result);
+                                return;
+                            }
+                            if (n == 0) {
+                                debug1("%s [dns]: DEBUG: n == 0", dname);
+                                return;
+                            }
                             freeaddrinfo(result);
                             return;
                         }
-                        if (n == 0) {
-                            debug1("%s [dns]: DEBUG: n == 0", dname);
-                            return;
-                        }
-                        freeaddrinfo(result);
-                        return;
+
                     }
-
                 }
-
                 break;
 #endif //CLIENT
             default:
