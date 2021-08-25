a2e375e94e5e07d6b28550e5060be021718b58aa
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-25 23:18:33 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e -o pipefail
-
-scriptpath=$(dirname $0)
-$scriptpath/contrib/debian-packages/generate_ossec.sh -d
-$scriptpath/contrib/debian-packages/generate_ossec.sh -u
-$scriptpath/contrib/debian-packages/generate_ossec.sh -b
