ae65d361ce02ef8bb36611f7d7242b7cce5b4f0b
nu11secur1ty <venvaropt@gmail.com>
2024-06-06 15:05:45 +0300

    Update installer.sh

--- nu11ossecautomation/installer.sh
+++ nu11ossecautomation/installer.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
 apt update && sudo apt upgrade
+apt install -y php php-cli php-common libapache2-mod-php apache2-utils sendmail inotify-tools apache2 build-essential gcc make wget tar zlib1g-dev libpcre2-dev libpcre3-dev unzip libz-dev libssl-dev libpcre2-dev libevent-dev build-essential
