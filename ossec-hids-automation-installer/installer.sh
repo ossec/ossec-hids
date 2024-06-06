e028b073233b1403fc6a79a697ebe7c5d2bfdc06
nu11secur1ty <venvaropt@gmail.com>
2024-06-06 17:40:21 +0300

    Update installer.sh

--- ossec-hids-automation-installer/installer.sh
+++ ossec-hids-automation-installer/installer.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+# nu11secur1ty 2024
+
 apt update && sudo apt upgrade
 apt install -y php php-cli php-common libapache2-mod-php apache2-utils sendmail inotify-tools apache2 build-essential gcc make wget tar zlib1g-dev libpcre2-dev libpcre3-dev unzip libz-dev libssl-dev libpcre2-dev libevent-dev libsystemd0 libsystemd-dev build-essential
 systemctl enable apache2
