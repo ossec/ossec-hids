2254d33c7d7db0ae610129e01ae9d32d81d48fea
nu11secur1ty <venvaropt@gmail.com>
2024-06-06 15:32:59 +0300

    Update installer.sh

--- nu11ossecautomation/installer.sh
+++ nu11ossecautomation/installer.sh
@@ -13,7 +13,7 @@ bash install.sh
 cd /tmp/
 git clone https://github.com/ossec/ossec-wui.git
 mv /tmp/ossec-wui /var/www/html
-  rm -rf /var/www/html/index.html
+  rm -rf /var/www/html/*
 cd /var/www/html/ossec-wui
 chown -R www-data:www-data /var/www/html/ossec-wui/
 chmod -R 755 /var/www/html/ossec-wui/
